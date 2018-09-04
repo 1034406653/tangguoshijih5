ae78ee4fba1c1af1f1a2934a80816295f8725d30
AU-Pro <905362865@qq.com>
2018-09-04 18:14:35 +0800

    'hanyu-09-04'

---
+++ src/components/center/set_question.vue
@@ -0,0 +1,35 @@
+<style scoped>
+  .pageContent {
+    width: 100%;
+    margin-top: -88px;
+  }
+
+  .pageContent img {
+    width: 100%;
+  }
+</style>
+
+<template>
+  <div class="content2">
+    <headerNav :pageTitle="pageTitle"></headerNav>
+    <div class="pageContent">
+      <img src="../../assets/img/center/question@2x.png" alt="">
+    </div>
+  </div>
+</template>
+
+<script>
+  import HeaderNav from '../base/headerNav'
+  export default {
+    data() {
+      return {
+        pageTitle: "产品介绍"
+      }
+    },
+    components: {
+      HeaderNav,
+    },
+  }
+</script>
+
+
